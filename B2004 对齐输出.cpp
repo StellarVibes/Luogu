5f134e52978b9c3c117b828707aabb8cc6be60ab
qhe32673-arch <qhe32673@gmail.com>
2025-11-12 14:02:32 +0800

    顺序结构

---
+++ B2004 对齐输出.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x=0,y=0,z=0;
+    cin>>x>>y>>z;
+    printf("%8d %8d %8d",x,y,z);
+    return 0;
+}
--- P1001 A+B Problem - 副本.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    int x=0,y=0;
-    cin>>x>>y;
-    cout<<x+y;
-}
