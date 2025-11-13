9f1232ea31b0773b1d7879831e8d55b978e99992
qhe32673-arch <qhe32673@gmail.com>
2025-11-13 13:47:50 +0800

    顺序结构

---
+++ B2023 空格分隔输出.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char x1=0;
+    int x2=0;
+    double x3=0,x4=0;
+    cin>>x1>>x2>>x3>>x4;
+    printf("%c %d %.6f %.6f",x1,x2,x3,x4);
+    return 0;
+}
