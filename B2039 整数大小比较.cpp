750b4525d55261f1dc2cd1b35700464f9cdf9a38
qhe32673-arch <qhe32673@gmail.com>
2025-11-15 15:57:06 +0800

    分支结构

---
+++ B2039 整数大小比较.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int x = 0,y = 0;
+    cin >> x>>y;
+    if(x>y) cout<<">";
+    else if(x==y) cout<<"=";
+    else cout<<"<";
+    return 0;
+}
