0a02252071e960b0bdc67488a43eff539cf30005
qhe32673-arch <qhe32673@gmail.com>
2025-11-15 11:50:53 +0800

    分支结构

---
+++ B2035 判断数正负.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 0;
+    cin >> n;
+    if(n>0) cout<<"positive";
+    else if(n==0) cout<<"zero";
+    else cout<<"negative";
+    return 0;
+}
