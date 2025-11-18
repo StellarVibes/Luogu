4fb1d19a687b127fd272b713f2186ea6547f17b3
qhe32673-arch <qhe32673@gmail.com>
2025-11-18 18:34:38 +0800

    分支结构

---
+++ B2052 简单计算器.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int x = 0,y= 0;
+    char ch = 0;
+    cin >>x>>y>>ch;
+    if(ch=='/'&&y==0) cout<<"Divided by zero!";
+    else if(!(ch=='+'||ch=='-'||ch=='*'||ch=='/')) cout<<"Invalid operator!";
+    else{
+        switch(ch)
+        {
+            case '+':cout<<x+y;break;
+            case '-':cout<<x-y;break;
+            case '*':cout<<x*y;break;
+            case '/':cout<<x/y;break;
+        }
+    }
+    return 0;
+}
