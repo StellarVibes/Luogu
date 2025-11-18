e3c1daa8cfb7f91e1f90d88511132e92c15ad94e
qhe32673-arch <qhe32673@gmail.com>
2025-11-18 18:25:12 +0800

    分支结构

---
+++ B2051 点和正方形的关系.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <climits>
+using namespace std;
+
+int main() {
+    int x = 0,y= 0;
+    cin >>x>>y;
+    if(x>=-1&&x<=1&&y>=-1&&y<=1) cout<<"yes";
+    else cout<<"no";
+    return 0;
+}
