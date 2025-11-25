11beab8cad3ded05c71d8c2d71a456bf58d22d97
qhe32673-arch <qhe32673@gmail.com>
2025-11-25 18:22:08 +0800

    数组

---
+++ B2089 数组逆序重存放.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n = 0;
+    int arr[100]={0};
+    cin>>n;
+    for(int i = 0;i<n;i++)
+    {
+        int num = 0;
+        cin>>num;
+        arr[i]=num;
+    }
+    for(int i = n-1;i>=0;i--)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
